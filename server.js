9ce2fdd3d8dedd368c75f3b4b11be8df2aebab22
nkleinmann <nekleinmann@gmail.com>
2020-10-13 20:42:44 -0400

    added routes and models folders

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
@@ -8,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
