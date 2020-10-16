66e541906f94209722f5c71d8eab3fcf1ae66e93
nkleinmann <nekleinmann@gmail.com>
2020-10-16 18:18:18 -0400

    Removed comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,20 +1,10 @@
 const express = require("express");
 const { Workout } = require("../models");
 
-// const router = express.Router();
-
 //importing object from index.js, models is an object
 const models = require("../models");
 
 module.exports = function (app) {
-    // app.post("/api/workouts", function (req, res) {
-    //     const workout = new models.Workout(new Date(), []);
-    //     workout.save(function (err, response) {
-    //         if (err) throw (err);
-    //         // response will return a json object
-    //         res.json(response);
-    //     });
-    // });
 
     app.post("/api/workouts", (req, res) => {
         Workout.create({})
--- server.js
+++ server.js
@@ -20,19 +20,14 @@ mongoose.connect( process.env.MONGODB_URI ,
   { useNewUrlParser: true,
   useUnifiedTopology: true })
   // used this resource to connect atlas https://medium.com/@sergio13prez/connecting-to-mongodb-atlas-d1381f184369
-// process.env.MONGODB_URI || "mongodb://localhost/populatedb"
+
 .then(() => {
   console.log("MongoDB Connected...")
 }).catch(err => console.log(err));
 
-// Go through code/ discuss plan and how to set up mongoose using atlas (have been using mongodb locally)
-
 // immediately invokes function
 require("./routes/htmlRoutes")(app);
 require("./routes/apiRoutes")(app);
-
-
-
 
 // Start the server
 app.listen(PORT, () => {
